f7f02a04cd0f01648334860258b97d6865b2b442
Michael Bortnyck <michael.bortnyck@gmail.com>
2018-05-08 09:01:06 -0500

    :: fix

--- ch3/22_zipAdd.scala
+++ ch3/22_zipAdd.scala
@@ -1,4 +1,4 @@
 def zipAdd(l1: List[Int], l2: List[Int]): List[Int] = {
-  if (l1.isEmpty || l2.isEmpty) List[Int]()
-  else List[Int](l1.head + l2.head) ++ zipAdd(l1.tail, l2.tail)
+  if (l1.isEmpty || l2.isEmpty) Nil
+  else (l1.head + l2.head) :: zipAdd(l1.tail, l2.tail)
 }
--- ch3/23_zipWith.scala
+++ ch3/23_zipWith.scala
@@ -1,4 +1,4 @@
 def zipWith[A](l1: List[A], l2: List[A])(f: (A, A) => A): List[A] = {
-  if (l1.isEmpty || l2.isEmpty) List[A]()
-  else List[A](f(l1.head, l2.head)) ++ zipWith(l1.tail, l2.tail)(f)
+  if (l1.isEmpty || l2.isEmpty) Nil
+  else f(l1.head, l2.head) :: zipWith(l1.tail, l2.tail)(f)
 }
