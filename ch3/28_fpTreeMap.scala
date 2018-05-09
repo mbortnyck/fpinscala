75b59c40ea906b661e38c9e51b6ec11b0626b720
Michael Bortnyck <michael.bortnyck@gmail.com>
2018-05-08 20:18:21 -0500

    3.28

---
+++ ch3/28_fpTreeMap.scala
@@ -0,0 +1,11 @@
+// 3.28: Write a function map, analogous to the method of the same name
+// on List, that modifies each element in a tree with a given function.
+
+sealed trait Tree[+A]
+case class Leaf[A](value: A) extends Tree[A]
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+def fpTreeMap[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+  case Branch(l, r) => Branch(fpTreeMap(l)(f), fpTreeMap(r)(f))
+  case Leaf(v) => Leaf(f(v))
+}
